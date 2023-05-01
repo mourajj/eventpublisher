59b3b6822f1b854c875717b89b6d6c7988d68358
Jonathan Henrique de Moura <jonathanmourax4@gmail.com>
2023-05-01 15:08:00 -0300

    small changes

--- main.go
+++ main.go
@@ -1,20 +1,11 @@
 package main
 
 import (
-	"eventpublisher/aws"
 	"fmt"
-	"log"
 	"time"
 )
 
 func main() {
-
-	awsSession, err := aws.CreateSession()
-	if err != nil {
-		log.Fatal(err)
-	}
-	aws.UploadFileToS3(awsSession, "testfile.txt", "testbucket")
-
 	for {
 		time.Sleep(3 * time.Second)
 		fmt.Println("Running on docker")

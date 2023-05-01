7d946192c6166b016ecb502f9a30e3384e6a8eb8
Jonathan Henrique de Moura <jonathanmourax4@gmail.com>
2023-05-01 11:22:28 -0300

    S3 upload unit test

--- aws/aws_test.go
+++ aws/aws_test.go
@@ -1,12 +1,22 @@
 package aws
 
 import (
+	"bytes"
 	"eventpublisher/constants"
+	"os"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3iface"
+	"github.com/golang/mock/gomock"
+	"github.com/xitongsys/parquet-go-source/s3/mocks"
 )
+
+type mockS3Client struct {
+	s3iface.S3API
+}
 
 func TestSessionCreation(t *testing.T) {
 
@@ -18,4 +28,39 @@ func TestSessionCreation(t *testing.T) {
 		t.Errorf("Failed to create AWS session: %s", err)
 	}
 
+}
+
+func TestUploadFileToS3(t *testing.T) {
+	//Creating an instance of the gomock controller
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	//Creating a mock of S3 client
+	mockS3Client := mocks.NewMockS3API(ctrl)
+
+	//Test Parameters
+	file := []byte("assuming this is a file")
+	bucket := "test-bucket"
+	key := "test-key"
+
+	//Creating an expected output for PutObject call
+	expectedOutput := &s3.PutObjectOutput{}
+
+	//Setting up the mock s3 client to expect a Putobject call
+	mockS3Client.EXPECT().PutObject(&s3.PutObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+		Body:   bytes.NewReader(file),
+	}).Return(expectedOutput, nil)
+
+	//Running the putobject call
+	_, err := mockS3Client.PutObject(&s3.PutObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+		Body:   bytes.NewReader(file),
+	})
+	if err != nil {
+		t.Errorf("Failed to upload a file to S3.  (%s)", err)
+		os.Exit(1)
+	}
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.20
 
 require (
 	github.com/aws/aws-sdk-go v1.44.253 // indirect
+	github.com/golang/mock v1.6.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/xitongsys/parquet-go-source v0.0.0-20230312005205-fbbcdea5f512 // indirect
 )
